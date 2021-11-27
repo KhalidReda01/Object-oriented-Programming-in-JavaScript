f7740363f6387db40689ccf99ebcf9424acee14f
khalidreda <khalidaziz2198@gmail.com>
2021-11-27 08:46:32 +0200

    cfm factory functions

--- cfm-js-oop/index.js
+++ cfm-js-oop/index.js
@@ -1,12 +1,16 @@
-const circle = {
-  raduis: 1,
-  location: {
-    x: 1,
-    y:1
-  },
-  draw: function () {
-    console.log(`I think this way is much better and yes print the output here you have to print draw  `)
-    
+
+  
+// cfm and I used const instead of declare the function with the name 
+// I know this may be silly but no one who say that why you commit evrey thing 
+// I will repeat every code even it is easy wait this is just the start of the course 
+const createCircle =function(raduis){
+  return {
+    raduis,
+     draw: function () {
+      console.log(`cfm circle `)
+      
+    }
   }
 }
+const circle = createCircle(3)
 circle.draw()
