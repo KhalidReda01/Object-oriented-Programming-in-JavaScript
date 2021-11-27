b401afeb5c1535dde6c5bd86dcca3263c9d67c46
khalidreda <khalidaziz2198@gmail.com>
2021-11-27 08:36:12 +0200

    factory function

--- js-oop/index.js
+++ js-oop/index.js
@@ -1,13 +1,23 @@
 // console.log("hello world ")
-const circle = {
-  raduis: 1,
-  location: {
-    x: 1,
-    y: 1
-  },
-  draw: function () {
-    console.log('draw')
-  }
+// const circle = {
+//   raduis: 1,
+//   location: {
+//     x: 1,
+//     y: 1
+//   },
+//   draw: function () {
+//     console.log('draw')
+//   }
 
-};
+// };
+function createCricle(raduis) {
+  return {
+    raduis,
+    draw: function () {
+      console.log("draw from factory function")
+    }
+  }
+}
+
+ const circle = createCricle(2);
 circle.draw()
