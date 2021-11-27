530ae3921b7100077e6d1a1d1ec44572e75634a1
khalidreda <khalidaziz2198@gmail.com>
2021-11-27 08:21:34 +0200

    code again but from my mind

---
+++ cfm-js-oop/index.js
@@ -0,0 +1,12 @@
+const circle = {
+  raduis: 1,
+  location: {
+    x: 1,
+    y:1
+  },
+  draw: function () {
+    console.log(`I think this way is much better and yes print the output here you have to print draw  `)
+    
+  }
+}
+circle.draw()
