526820aef54cea4186b69e2710cf86da49e48f79
khalidreda <khalidaziz2198@gmail.com>
2021-11-27 07:57:54 +0200

    object literals

--- js-oop/index.js
+++ js-oop/index.js
@@ -1,1 +1,13 @@
-console.log("hello world ")
+// console.log("hello world ")
+const circle = {
+  raduis: 1,
+  location: {
+    x: 1,
+    y: 1
+  },
+  draw: function () {
+    console.log('draw')
+  }
+
+};
+circle.draw()
